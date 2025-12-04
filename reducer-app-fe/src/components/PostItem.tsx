3a36531aa426c3d2266e56f0b9711353ae02de06
Eren Ergüven <erenerguven59@gmail.com>
2025-12-04 11:25:06 +0300

    User posts are grouped.

--- reducer-app-fe/src/components/PostItem.tsx
+++ reducer-app-fe/src/components/PostItem.tsx
@@ -2,8 +2,7 @@ import type { Post } from "./Post.types";
 
 export default function PostItem(props: Post) {
     return(
-        <div>
-            <h2>Post ID: {props.id}</h2>
+        <div className="postItem">
             <h4>Title: {props.title}</h4>
             <p>{props.body}</p>
         </div>
--- reducer-app-fe/src/components/PostList.tsx
+++ reducer-app-fe/src/components/PostList.tsx
@@ -1,23 +1,62 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState, type ReactNode } from "react";
 import PostItem from "./PostItem";
 import { PostContext } from "./PostContext";
+import type { Post } from "./Post.types";
+import UserPosts from "./UserPosts";
 
 export default function PostList() {
     const { posts, counter, fetchPosts } = useContext(PostContext);
+
+    const [filteredPosts, setFilteredPosts] = useState<Map<number,Post[]>>(new Map<number,Post[]>());
+  
+
+    function addPostToMap(post: Post) {
+        setFilteredPosts((prevMap) => {
+            const newMap = new Map(prevMap);
+            const existingPosts = newMap.get(post.userId);
+
+            if(existingPosts) { // Add post to list
+                const updatedPosts = [...existingPosts, post];
+                newMap.set(post.userId,updatedPosts);
+            }
+            else { // Add new key and create list
+                const newPosts = [post];
+                newMap.set(post.userId,newPosts);
+            }
+
+            return newMap;
+        });
+    }
 
     useEffect(() => {
         fetchPosts();
     }, [])
 
+    useEffect(() => {
+        posts.map((post) => {
+            addPostToMap(post)
+        })
+    },[posts])
+
     return(
-        <div style={{backgroundColor: "pink"}}>
-            <h1>-- FETCHED POSTS --</h1>
-            <h3>No. of Posts Available ({counter})</h3>
-            {posts.map((post) => {
-                return(
-                    <PostItem key={post.id} id={post.id} title={post.title} body={post.body}></PostItem>
-                )
-            })}
+        <div className="postListBackground">
+            <h1 className="titleHeader">FETCHED POSTS</h1>
+            <h3>Total No. of Posts Available ({counter})</h3>
+            <div className="postList">
+                {
+                    Array.from(filteredPosts.entries()).map(([userId, userPostsList]) => {
+                        return (
+                            <UserPosts userId={userId} userPostsCount={userPostsList.length} key={userId}>
+                                {userPostsList.map((post) => {
+                                    return(
+                                        <PostItem key={post.id} userId={post.userId} id={post.id} title={post.title} body={post.body}></PostItem>
+                                    )
+                                })}
+                            </UserPosts>
+                        );
+                    })
+                }
+            </div>
         </div>
     )
 }
